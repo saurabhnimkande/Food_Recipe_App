d07d3812d926b4694b59718e8ab7458f3e86cdc3
SaurabhN16 <91530878+saurabhnimkande@users.noreply.github.com>
2021-10-19 19:12:55 +0530

    added navbar to all the pages using import export

---
+++ Components/navbar.js
@@ -0,0 +1,10 @@
+function navbar () {
+    return ` <div><img src="https://www.pngitem.com/pimgs/m/395-3951211_my-recipes-logo-png-transparent-my-recipes-logo.png"></div>
+    <div onclick="indexPage()">Home</div>
+    <div onclick="recipeofthedayPage()">Recipe of The Day</div>
+    <div onclick="latestPage()">Latest Recipe</div>   
+    <div>About</div>  `
+}
+
+
+export default navbar;
---
+++ scripts/navbar._onclick.js
@@ -0,0 +1,11 @@
+function recipeofthedayPage() {
+    window.location.href = "recipeoftheday.html";;
+}
+function latestPage() {
+    window.location.href="latestrecipe.html";
+}
+function indexPage() {
+    window.location.href = "index.html";
+}
+
+console.log(one);
