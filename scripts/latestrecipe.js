0cbcc5d8c702028370add01bc5a7782c419e1bc1
SaurabhN16 <91530878+saurabhnimkande@users.noreply.github.com>
2021-10-20 08:42:50 +0530

    latest recipe page added with fetch

---
+++ scripts/latestrecipe.js
@@ -0,0 +1,37 @@
+
+    let parent = document.getElementById("container")
+
+    async function getData() {
+
+        let res= await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood`);
+      let data=await res.json();
+      console.log('data:', data.meals);
+      appendDiv(data.meals)
+
+    }
+    getData();
+
+    function appendDiv(data) {
+      data.forEach(({idMeal,strMeal,strMealThumb})=>{
+
+        let div=document.createElement("div");
+
+        let img=document.createElement("img");
+        img.src=strMealThumb
+
+        let h4=document.createElement("h4");
+        h4.innerText=strMeal;
+
+        let p=document.createElement("p");
+        p.innerText="Id : "+idMeal;
+
+
+        div.append(img,h4,p);
+
+        parent.append(div);
+
+
+
+      })
+    }
+
